938989cdf2cb517b03f76eb3f31ffae686910e81
WalterJuarez <wjuarezm@miumg.edu.gt>
2022-07-16 15:57:51 -0600

    nuevo3

--- validation.js
+++ validation.js
@@ -1,1 +1,11 @@
 //admin, admin
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username.toUpperCase()=='ADMIN' && username == 'admin'){
+        alert('que la fuerza te acompañe');
+    }
+    else{
+        alert('algo anda mal');
+    }
+}
